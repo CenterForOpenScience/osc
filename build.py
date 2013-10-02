be047cf102e6b63c7dde62e5e189fced630a95da
Jeffrey Spies <jspies@gmail.com>
2013-10-02 17:45:44 -0400

    build script

---
+++ build.py
@@ -0,0 +1,47 @@
+from pelican import Pelican
+import pelican.settings
+
+import os
+
+settings = pelican.settings.DEFAULT_CONFIG
+
+for i in ['PATH', 'OUTPUT_PATH', 'THEME', 'SITEURL']:
+    del settings[i]
+
+settings.update({
+    u'THEME_STATIC_DIR':'theme',
+    u'AUTHOR': '',
+    u'SITENAME': 'Open Science Collaboration Blog',
+    u'TIMEZONE': 'America/New_York',
+    u'LINKS': (
+    ),
+    u'SOCIAL' : (
+    ),
+})
+
+settings['ARTICLE_URL'] = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+settings['ARTICLE_SAVE_AS'] = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+settings['DISQUS_SITENAME'] = 'opensciencecollaboration'
+
+def generate(settings, input_path=None, output_path=None, theme_path=None, siteurl=None ):
+    settings['PATH'] = input_path
+    settings['OUTPUT_PATH'] =  output_path
+    settings['THEME'] = theme_path
+    settings['SITEURL'] = siteurl
+
+    if settings['SITEURL'].endswith('/'):
+        settings['SITEURL'] = settings['SITEURL'][:-1]
+
+    if not os.path.exists(settings['OUTPUT_PATH']):
+        os.mkdir(settings['OUTPUT_PATH'])
+
+    settings = pelican.settings.configure_settings(settings)
+    Pelican(settings).run()
+
+generate(
+    settings,
+    input_path='site/content',
+    output_path='output/',
+    theme_path='pelican-mockingbird/',
+    siteurl='http://osc.centerforopenscience.org'
+)
