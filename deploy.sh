7a82d51e3f26631901fe8a00cb3f65f57cb08469
Shauna <shaunagm@gmail.com>
2014-07-25 19:17:44 -0400

    Switch content to site in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e   # if there's an error, stop running
 set -x   # print each command as it runs
-pelican content -s build.py
+pelican site -s build.py
 ghp-import output -m "Regenerate site"
 git checkout gh-pages
 echo "osc.centerforopenscience.org" > CNAME
